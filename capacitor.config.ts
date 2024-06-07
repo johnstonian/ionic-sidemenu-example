847c4501778440fabc21803d8d6a62a471e82128
Ryan Johnston <ryanjohnston@gmail.com>
2024-06-07 09:00:31 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-sidemenu-example',
+  webDir: 'www'
+};
+
+export default config;
